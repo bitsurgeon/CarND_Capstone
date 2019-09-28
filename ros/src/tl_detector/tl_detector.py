6fd8a0c7068254fe4f1fa59f778efaab7060bc20
Yan Zhang <yanyanoo101@hotmail.com>
2019-09-28 14:28:53 +0100

    feat: to integrate trained model

--- ros/src/tl_detector/tl_detector.py
+++ ros/src/tl_detector/tl_detector.py
@@ -48,7 +48,7 @@ class TLDetector(object):
         self.upcoming_red_light_pub = rospy.Publisher('/traffic_waypoint', Int32, queue_size=1)
 
         self.bridge = CvBridge()
-        self.light_classifier = TLClassifier()
+        self.light_classifier = TLClassifier(True)
         self.listener = tf.TransformListener()
 
         self.has_image = False
@@ -144,16 +144,11 @@ class TLDetector(object):
             return False
 
         cv_image = self.bridge.imgmsg_to_cv2(self.camera_image, "bgr8")
-
-        if light.state == TrafficLight.YELLOW:
-            cv2.imwrite('sim_images/yellow/cam_img_' + str(self.img_cnt) + '.png', cv_image)
-        if light.state == TrafficLight.RED:
-            cv2.imwrite('sim_images/red/cam_img_' + str(self.img_cnt) + '.png', cv_image)
-        if light.state == TrafficLight.GREEN:
-            cv2.imwrite('sim_images/green/cam_img_' + str(self.img_cnt) + '.png', cv_image)
         
         #Get classification
-        return self.light_classifier.get_classification(cv_image)
+        color = self.light_classifier.get_classification(cv_image, True)
+	rospy.loginfo("traffic light color from classification is: %d", color)	
+	return color
 
     def process_traffic_lights(self):
         """Finds closest visible traffic light, if one exists, and determines its
