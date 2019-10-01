8a757ca278b56cd21fdecc1602ee1a9d12cdf96a
Yongzhi <43927708+bitsurgeon@users.noreply.github.com>
2019-10-01 12:15:48 +1000

    Revert "integrate inference model for site cam classification"

--- ros/src/tl_detector/light_classification/tl_classifier.py
+++ ros/src/tl_detector/light_classification/tl_classifier.py
@@ -5,22 +5,30 @@ import cv2
 import rospy
 import time
 
-# Set inference graph files.
-SSD_GRAPH_FILE_SITE = '../model/site_ssd_inception_v2_coco_2018_01_28_39747_v1_4/frozen_inference_graph.pb'
-SSD_GRAPH_FILE_SIMULATOR = '../model/sim_ssd_inception_v2_coco_2018_01_28_xxxxx_v1_4/frozen_inference_graph.pb'
+LIGHTS = ['Green', 'Yellow', 'Red', 'Unknown']
 
 class TLClassifier(object):
     def __init__(self, is_site):
-        # Load inference graph.
         if is_site:
-            self.graph = self.load_graph(SSD_GRAPH_FILE_SITE)
+            PATH_TO_FROZEN_GRAPH = 'light_classification/model/site_model/'
         else:
-            self.graph = self.load_graph(SSD_GRAPH_FILE_SIMULATOR)
+            PATH_TO_FROZEN_GRAPH = 'light_classification/model/sim_model/'
+        FROZEN_GRAPH = PATH_TO_FROZEN_GRAPH + 'frozen_inference_graph.pb'
+        rospy.loginfo("loading classification graph............")
+        self.graph = tf.Graph()
+        with self.graph.as_default():
+            od_graph_def = tf.GraphDef()
+            with tf.gfile.GFile(FROZEN_GRAPH, 'rb') as fid:
+                serialized_graph = fid.read()
+                od_graph_def.ParseFromString(serialized_graph)
+                tf.import_graph_def(od_graph_def, name='')
         
-        self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
-        self.detection_boxes = self.graph.get_tensor_by_name('detection_boxes:0')
-        self.detection_scores = self.graph.get_tensor_by_name('detection_scores:0')
-        self.detection_classes = self.graph.get_tensor_by_name('detection_classes:0')
+            self.image_tensor = self.graph.get_tensor_by_name('image_tensor:0')
+            self.detection_boxes = self.graph.get_tensor_by_name('detection_boxes:0')
+            self.detection_scores = self.graph.get_tensor_by_name('detection_scores:0')
+            self.detection_classes = self.graph.get_tensor_by_name('detection_classes:0')
+        rospy.loginfo("loaded graph............")
+        self.sess = tf.Session(graph=self.graph)
 
     def filter_boxes(self, min_score, boxes, scores, classes):
         """Return boxes with a confidence >= `min_score`"""
@@ -35,28 +43,18 @@ class TLClassifier(object):
         filtered_classes = classes[idxs, ...]
         return filtered_boxes, filtered_scores, filtered_classes
 
-    def load_graph(self, graph_file):
-        """Loads a frozen inference graph"""
-        graph = tf.Graph()
-        with graph.as_default():
-            od_graph_def = tf.GraphDef()
-            with tf.gfile.GFile(graph_file, 'rb') as fid:
-                serialized_graph = fid.read()
-                od_graph_def.ParseFromString(serialized_graph)
-                tf.import_graph_def(od_graph_def, name='')
 
-        return graph
-
-    def get_classification(self, image, confidence_cutoff=0.7):
+    def get_classification(self, image):
         """Determines the color of the traffic light in the image
         Args:
             image (cv::Mat): image containing the traffic light
         Returns:
             int: ID of traffic light color (specified in styx_msgs/TrafficLight)
         """
+        image = np.dstack((image[:, :, 2], image[:, :, 1], image[:, :, 0]))
         image_np = np.expand_dims(np.asarray(image, dtype=np.uint8), 0)
 
-        with tf.Session(graph=self.graph) as sess:
+        with tf.Session(graph=self.graph) as sess:                
             # Actual detection
             time0 = time.time()
             (boxes, scores, classes) = sess.run([self.detection_boxes, self.detection_scores, self.detection_classes], 
@@ -69,32 +67,24 @@ class TLClassifier(object):
             boxes = np.squeeze(boxes)
             scores = np.squeeze(scores)
             classes = np.squeeze(classes)
-
+        
+            confidence_cutoff = 0.7
             # Filter boxes with a confidence score less than `confidence_cutoff`
             boxes, scores, classes = self.filter_boxes(confidence_cutoff, boxes, scores, classes)
         
-        if len(classes) > 0:
+        
+        if len(classes)>0:
             color_state = int(classes[np.argmax(scores)])
             
             if color_state == 1:
-                rospy.loginfo("traffic light color from classification is GREEN")	
-                return TrafficLight.GREEN
+		 rospy.loginfo("traffic light color from classification is GREEN")	
+                 return TrafficLight.GREEN
             elif color_state == 2:
-                rospy.loginfo("traffic light color from classification is RED")
-                return TrafficLight.RED
+		 rospy.loginfo("traffic light color from classification is RED")
+                 return TrafficLight.RED
             elif color_state == 3:
-                rospy.loginfo("traffic light color from classification is YELLOW")
-                return TrafficLight.YELLOW
-        
+		 rospy.loginfo("traffic light color from classification is YELLOW")
+                 return TrafficLight.YELLOW
         rospy.loginfo("traffic light color from classification is UNKNOWN")                    
         return TrafficLight.UNKNOWN
 
-if __name__ == '__main__':
-
-    from PIL import Image
-
-    image = Image.open('../model/rosbag_sample.jpg')
-
-    isSite = True
-
-    print TLClassifier(isSite).get_classification(image)
